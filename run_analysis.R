9d02d4f64dff64c5f20cf923a47b94e1db982bee
Utsav Gupta <34511793+utsavgpt5@users.noreply.github.com>
2018-07-10 09:47:39 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,6 @@
-if(!file.exists("./data")){dir.create("./data")}fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"download.file(fileUrl,destfile="./data/Dataset.zip")
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
 # Unzip dataSet to /data directory
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 # Reading trainings tables:
